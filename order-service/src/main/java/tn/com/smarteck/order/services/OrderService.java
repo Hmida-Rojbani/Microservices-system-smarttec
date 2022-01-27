033427c054846cdfb6732761bfbf5b2d367a8761
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2022-01-27 12:21:35 +0100

    add discovery

--- order-service/src/main/java/tn/com/smarteck/order/services/OrderService.java
+++ order-service/src/main/java/tn/com/smarteck/order/services/OrderService.java
@@ -23,7 +23,7 @@ public class OrderService {
 	public Order saveOrder(Order order) {
 		//do a rest call to payment and pass the order id;
 		TransactionResPayment trp = restTemplate
-				.postForObject("http://localhost:9191/api/payments/doPayment", 
+				.postForObject("http://PAIMENT-SERVICE/api/payments/doPayment", 
 				new TransactionReqOrder(order.getId(),order.getPrice()*order.getQty()), TransactionResPayment.class);
 		if(trp.getPaymentStatus().equals("success")) {
 			order.setTransactionId(trp.getTransactionId());
