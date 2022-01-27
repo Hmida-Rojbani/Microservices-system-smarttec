fe2cc55283277fc60ed9e863618f3131c0e6c538
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2022-01-27 15:15:33 +0100

    useing resilience for fault tolerance

--- order-service/src/main/java/tn/com/smarteck/order/controlles/OrderCtrl.java
+++ order-service/src/main/java/tn/com/smarteck/order/controlles/OrderCtrl.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.AllArgsConstructor;
 import tn.com.smarteck.order.entities.Order;
 import tn.com.smarteck.order.services.OrderService;
@@ -17,9 +18,9 @@ import tn.com.smarteck.order.services.OrderService;
 public class OrderCtrl {
 	
 	private OrderService orderService;
-	private ServletWebServerApplicationContext webServerAppCtxt;
 	
 	@PostMapping("/bookOrder")
+	
 	public Order bookOrder(@RequestBody Order order) {
 		return orderService.saveOrder(order);
 		
@@ -27,7 +28,7 @@ public class OrderCtrl {
 	
 	@GetMapping("/hello")
 	public String hello() {
-		return "Hello from Order Service :" + webServerAppCtxt.getWebServer().getPort();
+		return "Hello from Order Service ";
 	}
 
 }
--- order-service/src/main/java/tn/com/smarteck/order/services/OrderService.java
+++ order-service/src/main/java/tn/com/smarteck/order/services/OrderService.java
@@ -3,6 +3,7 @@ package tn.com.smarteck.order.services;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.AllArgsConstructor;
 import tn.com.smarteck.order.dto.TransactionReqOrder;
 import tn.com.smarteck.order.dto.TransactionResPayment;
@@ -17,9 +18,12 @@ public class OrderService {
 
 	private OrderRepository orderRepository;
 	private RestTemplate restTemplate;
+	
+	private static final String ORDER_SERVICE= "orderService";
 	//feignClient
 	private PaimentServiceClient paimentServiceClient;
 	//with Rest Template
+	@CircuitBreaker(name= ORDER_SERVICE,fallbackMethod = "getFailedPaimentResponse")
 	public Order saveOrder(Order order) {
 		//do a rest call to payment and pass the order id;
 		TransactionResPayment trp = restTemplate
@@ -31,6 +35,12 @@ public class OrderService {
 	}
 		else 
 			throw new PaimentFailedException("payment failed Order declined");
+	}
+	
+	public Order getFailedPaimentResponse(Exception e) {
+		Order order = new Order();
+		order.setTransactionId("Paiment system failed to response");
+		return order;
 	}
 	
 	//with OpenFeign
